2df360da15a043e3afda2223c531c15aaccbe5f6
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-09-19 18:39:46 +0900

    Feat/issue #5 (#6)
    * FE 개발 환경 세팅 및 폴더구조 세팅 (#2) (#3) (#4)
    
    * Init: 초기 파일 세팅 및 폴더구조 생성
    
    * Docs(template): PR, Issue Template 추가
    
    * Feat(global-style): global font 및 color 선언
    
    * Feat(global-style): global style 적용
    
    * Feat(global-style): 웹앱 크기 Layout 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { queryClient } from './api/instance';
 import { Routes } from './routes';
-import { ChakraProvider } from '@chakra-ui/react';
+import GlobalTheme from './styles/global';
+import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
   return (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Routes />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={GlobalTheme}>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <Routes />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
   );
 };
 
