73c05c7c02a3f8414799f34e450eed00249bb74b
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-09-19 15:51:16 +0900

    FE 개발 환경 세팅 및 폴더구조 세팅 (#2) (#3)
    * Init: 초기 파일 세팅 및 폴더구조 생성
    
    * Docs(template): PR, Issue Template 추가

---
+++ src/api/instance/index.ts
@@ -0,0 +1,33 @@
+import type { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios from 'axios';
+
+import { QueryClient } from '@tanstack/react-query';
+
+const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
+  const instance = axios.create({
+    timeout: 5000,
+    ...config,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...config.headers,
+    },
+  });
+
+  return instance;
+};
+
+export const fetchInstance = initInstance({
+  baseURL: 'https://api.example.com',
+});
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnMount: true,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: true,
+    },
+  },
+});
