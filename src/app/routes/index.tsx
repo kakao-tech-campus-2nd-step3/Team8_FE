bdc41ff994042669de1f410c1aff9ddb238520e8
Dobbymin <gmin0701@knu.ac.kr>
2024-10-04 13:32:20 +0900

    Refactor(router): 서비스 이용 내역 페이지 경로 네이밍 변경

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { RouterPath } from './path';
+import MainPage from '@/pages/common/main';
+import RegisterPage from '@/pages/common/register';
+import ServiceHistoryPage from '@/pages/guard/service-history';
+import MyPage from '@/pages/siniddo/mypage';
+
+const router = createBrowserRouter([
+  {
+    path: RouterPath.root,
+    element: <MainPage />,
+  },
+  {
+    path: RouterPath.register,
+    element: <RegisterPage />,
+  },
+  {
+    path: RouterPath.mypage,
+    element: <MyPage />,
+  },
+  {
+    path: RouterPath.serviceHistory,
+    element: <ServiceHistoryPage />,
+  },
+]);
+
+export const Routes = () => {
+  return <RouterProvider router={router} />;
+};
