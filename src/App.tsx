668483dc151e4d025cde821de53893fd5d516073
Dobbymin <gmin0701@knu.ac.kr>
2024-10-04 14:19:41 +0900

    Refactor: global style chakra UI ->  emotion 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { Routes } from './app/routes';
 import { queryClient } from './shared/api/instance';
-import GlobalTheme from './shared/theme/global';
-import { ChakraProvider, GlobalStyle } from '@chakra-ui/react';
+import { globalStyle } from './shared/theme/global';
+import { ChakraProvider } from '@chakra-ui/react';
+import { Global } from '@emotion/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
   return (
-    <>
-      <ChakraProvider theme={GlobalTheme}>
-        <QueryClientProvider client={queryClient}>
-          <GlobalStyle />
-          <Routes />
-        </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Global styles={globalStyle} />
+        <Routes />
       </ChakraProvider>
-    </>
+    </QueryClientProvider>
   );
 };
 
--- src/shared/theme/global/index.ts
+++ src/shared/theme/global/index.ts
@@ -1,52 +1,66 @@
-import { extendTheme } from '@chakra-ui/react';
+import { css } from '@emotion/react';
 
-const GlobalTheme = extendTheme({
-  // Font Family 선언
-  fonts: {
-    body: `'Pretendard-Regular', sans-serif`,
-  },
-  colors: {
-    /**
-     * Global Color 선언
-     * 사용 : 'color.색상이름'
-     */
-    color: {
-      basic: '#2e2e2e',
-      white: '#fff',
-      gray: '#909090',
-      primary: '#C69090',
-      secondary: '#F6E4E4',
-      whiteGray: '#F2F2F2',
-    },
-  },
-  styles: {
-    // Global Font Face 선언
-    global: {
-      '@font-face': {
-        fontFamily: 'Pretendard-Regular',
-        src: `url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff')`,
-        fontWeight: '400, 700, 900',
-        fontStyle: 'normal',
-      },
-      body: {
-        margin: 0,
-        padding: 0,
-        boxSizing: 'border-box',
-        overflowY: 'auto',
-        width: '100%',
-        height: '100%',
-        position: 'relative',
+// fontSize 정의
+const fontSizeXXl = '1.5rem'; // 24px
+const fontSizeXl = '1.25rem'; // 20px
+const fontSizeLg = '1.175rem'; // 18px
+const fontSizeMd = '1rem'; // 16px
+const fontSizeSm = '0.875rem'; // 14px
+const fontSizeXs = '0.75rem'; // 12px
 
-        // 모바일 너비 적용
-        '@media(min-width: 768px)': {
-          width: '480px',
-        },
-      },
-      '*': {
-        boxSizing: 'border-box', // 모든 요소에 box-sizing 적용
-      },
-    },
-  },
-});
+// Global Color 정의
+const colorBlack = '#2e2e2e';
+const colorWhite = '#FFFFFF';
+const colorGray = '#909090';
+const colorPrimary = '#C69090';
+const colorSecondary = '#F6E4E4';
+const colorWhiteGray = '#F2F2F2';
 
-export default GlobalTheme;
+// Global Style 정의
+export const globalStyle = css`
+  :root {
+    --font-size-xxl: ${fontSizeXXl};
+    --font-size-xl: ${fontSizeXl};
+    --font-size-lg: ${fontSizeLg};
+    --font-size-md: ${fontSizeMd};
+    --font-size-sm: ${fontSizeSm};
+    --font-size-xs: ${fontSizeXs};
+
+    --color-basic: ${colorBlack};
+    --color-white: ${colorWhite};
+    --color-gray: ${colorGray};
+    --color-primary: ${colorPrimary};
+    --color-secondary: ${colorSecondary};
+    --color-white-gray: ${colorWhiteGray};
+  }
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+      format('woff');
+    font-weight: 400, 700, 900;
+    font-style: normal;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Pretendard-Regular', sans-serif;
+    overflow-y: auto;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  // 모바일 너비 적용
+  @media (min-width: 768px) {
+    body {
+      width: 480px;
+    }
+  }
+`;
