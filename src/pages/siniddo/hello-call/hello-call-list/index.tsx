cd27bab0210427d07b033548c1fa6b8e319b0757
Dobbymin <gmin0701@knu.ac.kr>
2024-10-05 10:42:40 +0900

    Feat(hello-call): 안부 전화 서비스 페이지 이동 기능 구현

--- src/pages/siniddo/hello-call/hello-call-list/index.tsx
+++ src/pages/siniddo/hello-call/hello-call-list/index.tsx
@@ -1,20 +1,29 @@
+import { useNavigate } from 'react-router-dom';
+
 import RequestList from './components/request-list';
+import { RouterPath } from '@/app/routes/path';
 import styled from '@emotion/styled';
 
-const HelloCallPage = () => {
+const HelloCallListPage = () => {
+  const navigate = useNavigate();
+
+  const handlerNavigate = () => {
+    navigate(RouterPath.helloCallService);
+  };
+
   return (
-    <HelloCallPageLayout>
-      <RequestList />
-      <RequestList />
-      <RequestList />
-      <RequestList />
-    </HelloCallPageLayout>
+    <HelloCallListLayout>
+      <RequestList onClick={handlerNavigate} />
+      <RequestList onClick={handlerNavigate} />
+      <RequestList onClick={handlerNavigate} />
+      <RequestList onClick={handlerNavigate} />
+    </HelloCallListLayout>
   );
 };
 
-export default HelloCallPage;
+export default HelloCallListPage;
 
-const HelloCallPageLayout = styled.div`
+const HelloCallListLayout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
