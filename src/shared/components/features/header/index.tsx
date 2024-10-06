e3f4fd3adb0a48057bdc2fc550b243c332b6b995
diwoni <jgw117@naver.com>
2024-10-06 10:52:06 +0900

    Feat: 공통 컴포넌트 헤더 구현

--- src/shared/components/features/header/index.tsx
+++ src/shared/components/features/header/index.tsx
@@ -1,9 +1,40 @@
-const Header = () => {
+import backIcon from './asset/ic_arrow_back.svg';
+import { Text, Image } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+interface HeaderProps {
+  title: string;
+}
+
+const Header = ({ title }: HeaderProps) => {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderBox>
+      <Icon src={backIcon} />
+      <Text fontSize='1.5rem' fontWeight={700}>
+        {title}
+      </Text>
+    </HeaderBox>
   );
 };
 
 export default Header;
+
+const HeaderBox = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled(Image)`
+  position: absolute;
+  width: 37px;
+  height: 37px;
+  left: 40px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
