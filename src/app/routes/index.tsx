79f0705f1f2c980e98a72d4e3c27f4d634f11b00
diwoni <jgw117@naver.com>
2024-10-08 21:24:37 +0900

    Feat: 헤더 포함한 레이아웃 컴포넌트 추가 및 UI 수정

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -15,6 +15,7 @@ import HelloCallListPage from '@/pages/siniddo/hello-call/hello-call-list';
 import HelloCallReportPage from '@/pages/siniddo/hello-call/hello-call-report';
 import HelloCallServicePage from '@/pages/siniddo/hello-call/hello-call-service';
 import SinittoMypage from '@/pages/siniddo/mypage';
+import Layout from '@/shared/components/common/layout';
 
 const router = createBrowserRouter([
   {
@@ -22,71 +23,47 @@ const router = createBrowserRouter([
     element: <MainPage />,
   },
   {
-    path: RouterPath.REGISTER,
-    element: <RegisterPage />,
-  },
-  {
-    path: RouterPath.SINITTO_MYPAGE,
-    element: <SinittoMypage />,
-  },
-  {
-    path: RouterPath.GUARD_MYPAGE,
-    element: <GuardMyPage />,
-  },
-  {
-    path: RouterPath.GUARD_GUIDELINE,
-    element: <GuideLinePage />,
-  },
-  {
-    path: RouterPath.SERVICE_HISTORY,
-    element: <ServiceHistoryPage />,
-  },
-  {
-    path: RouterPath.HELLO_CALL,
+    path: RouterPath.ROOT,
+    element: <Layout />,
     children: [
+      { path: RouterPath.REGISTER, element: <RegisterPage /> },
+      { path: RouterPath.SINITTO_MYPAGE, element: <SinittoMypage /> },
+      { path: RouterPath.GUARD_MYPAGE, element: <GuardMyPage /> },
+      { path: RouterPath.GUARD_GUIDELINE, element: <GuideLinePage /> },
+      { path: RouterPath.SERVICE_HISTORY, element: <ServiceHistoryPage /> },
       {
-        index: true,
-        element: <HelloCallListPage />,
-      },
-      {
-        path: RouterPath.HELLO_CALL_SERVICE,
-        element: <HelloCallServicePage />,
-      },
-      {
-        path: RouterPath.HELLO_CALL_REPORT,
-        element: <HelloCallReportPage />,
-      },
-    ],
-  },
-  {
-    path: RouterPath.SENIOR_REGISTER,
-    element: <SeniorRegisterPage />,
-  },
-  {
-    path: RouterPath.CALL_BACK_LIST,
-    children: [
-      {
-        index: true,
-        element: <CallBackListPage />,
-      },
-      {
-        path: RouterPath.CALL_BACK_DETAIL,
+        path: RouterPath.HELLO_CALL,
         children: [
+          { index: true, element: <HelloCallListPage /> },
           {
-            index: true,
-            element: <CallBackDetailPage />,
+            path: RouterPath.HELLO_CALL_SERVICE,
+            element: <HelloCallServicePage />,
           },
           {
-            path: RouterPath.CALL_BACK_GUID_LINE,
-            element: <SiniddoGuideLinePage />,
+            path: RouterPath.HELLO_CALL_REPORT,
+            element: <HelloCallReportPage />,
           },
         ],
       },
+      { path: RouterPath.SENIOR_REGISTER, element: <SeniorRegisterPage /> },
+      {
+        path: RouterPath.CALL_BACK_LIST,
+        children: [
+          { index: true, element: <CallBackListPage /> },
+          {
+            path: RouterPath.CALL_BACK_DETAIL,
+            children: [
+              { index: true, element: <CallBackDetailPage /> },
+              {
+                path: RouterPath.CALL_BACK_GUID_LINE,
+                element: <SiniddoGuideLinePage />,
+              },
+            ],
+          },
+        ],
+      },
+      { path: RouterPath.SINIDDO_REVIEW, element: <SiniddoReviewPage /> },
     ],
-  },
-  {
-    path: RouterPath.SINIDDO_REVIEW,
-    element: <SiniddoReviewPage />,
   },
 ]);
 
