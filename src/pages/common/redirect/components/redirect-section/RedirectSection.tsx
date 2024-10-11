1ea56ec310ac89a9e49a70af5f5cc02d9707599f
Dobbymin <gmin0701@knu.ac.kr>
2024-10-12 02:19:05 +0900

    Feat(login): 카카오 로그인 redirect page 기능 구현

---
+++ src/pages/common/redirect/components/redirect-section/RedirectSection.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useGetKakaoCallback } from '../../store/hooks/useGetKakaoCallback';
+import { Flex, Spinner, Text } from '@chakra-ui/react';
+
+type Props = {
+  code: string;
+};
+
+const RedirectSection = ({ code }: Props) => {
+  const navigate = useNavigate();
+
+  const { data } = useGetKakaoCallback(code);
+
+  useEffect(() => {
+    if (data) {
+      const accessToken = data.accessToken;
+      const refreshToken = data.refreshToken;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+
+      navigate('/');
+    }
+  }, [data, navigate]);
+
+  return (
+    <Flex
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+      height='100vh'
+      gap='2rem'
+    >
+      <Spinner />
+      <Text>로그인 진행중!</Text>
+    </Flex>
+  );
+};
+
+export default RedirectSection;
---
+++ src/pages/common/redirect/ui/RedirectPage.tsx
@@ -0,0 +1,16 @@
+import { useLocation } from 'react-router-dom';
+
+import RedirectSection from '../components/redirect-section/RedirectSection';
+
+const RedirectPage = () => {
+  const location = useLocation();
+
+  const code = new URLSearchParams(location.search).get('code');
+  if (!code) {
+    return <div>로그인을 다시 진행해주세요.</div>;
+  }
+
+  return <RedirectSection code={code} />;
+};
+
+export default RedirectPage;
