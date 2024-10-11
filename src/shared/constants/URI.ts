2fef21216cc852b76f9173cc4977eb44d17ea2f8
Dobbymin <gmin0701@knu.ac.kr>
2024-10-12 02:18:22 +0900

    Feat: URI 경로 상수화

---
+++ src/shared/constants/URI.ts
@@ -0,0 +1,5 @@
+export const BASE_URI = import.meta.env.VITE_APP_BASE_URL;
+
+export const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
+
+export const KAKAO_AUTH_URL: string = `${BASE_URI}/api/auth/oauth/kakao`;
