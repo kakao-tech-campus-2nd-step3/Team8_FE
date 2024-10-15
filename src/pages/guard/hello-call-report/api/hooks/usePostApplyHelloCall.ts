5e2aaa960f5864c6ac71f3ef50d6ebbf18a5edec
Dobbymin <gmin0701@knu.ac.kr>
2024-10-16 00:16:06 +0900

    Remove: reload 코드 제거

--- src/pages/guard/hello-call-report/api/hooks/usePostApplyHelloCall.ts
+++ src/pages/guard/hello-call-report/api/hooks/usePostApplyHelloCall.ts
@@ -12,7 +12,6 @@ export const usePostApplyHelloCall = (): UseMutationResult<
     mutationFn: (data: ApplyHelloCallRequest) => postApplyHelloCall(data),
     onSuccess: () => {
       alert('안부전화 서비스가 신청되었습니다.');
-      window.location.reload();
     },
     onError: (error) => {
       console.error('안부전화 서비스 신청에 실패했습니다.', error);
--- src/pages/guard/hello-call-report/api/hooks/usePostCostHelloCall.ts
+++ src/pages/guard/hello-call-report/api/hooks/usePostCostHelloCall.ts
@@ -12,7 +12,6 @@ export const usePostCostHelloCall = (): UseMutationResult<
     mutationFn: (request: CostHelloCallRequest) => postCostHelloCall(request),
     onSuccess: () => {
       alert('안부전화 서비스가 신청되었습니다.');
-      window.location.reload();
     },
     onError: (error) => {
       console.error('안부전화 서비스 신청에 실패했습니다.', error);
