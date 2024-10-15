ff7782db0756185e6eb5dc2d6447f10f6c5906ba
Dobbymin <gmin0701@knu.ac.kr>
2024-10-15 15:10:00 +0900

    Feat(hello-call): 수락한 안부전화 리스트 조회 api 호출 기능 구현

--- src/pages/guard/hello-call-report/api/index.ts
+++ src/pages/guard/hello-call-report/api/index.ts
@@ -1,5 +1,7 @@
-export * from './hooks';
-export * from './types';
-
 export { getReport } from './report.api';
 export { getTimeLog } from './time-log.api';
+export { getAcceptCallList } from './accept-call-list.api';
+export { getApplyCallList } from './apply-call-list.api';
+
+export * from './hooks';
+export * from './types';
