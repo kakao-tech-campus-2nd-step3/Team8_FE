bcbe811a20dce8f10438f8cc05de9765bcc29ac1
Dobbymin <gmin0701@knu.ac.kr>
2024-10-17 01:04:32 +0900

    Fix(hello-call-apply): 서비스 이용 시간 상수화

--- src/pages/guard/hello-call-apply/data/index.ts
+++ src/pages/guard/hello-call-apply/data/index.ts
@@ -1,3 +1,4 @@
 export { NOTICE_DATA } from './notice-data';
 export { DAY_DATA } from './day-data';
 export { TIME_SLOTS } from './time-slots-data';
+export { USE_TIME_DATA } from './use-time-data';
