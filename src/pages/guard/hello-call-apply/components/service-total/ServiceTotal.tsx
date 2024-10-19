2ca0256b28796160f1b87d01fb2b4e86151b9eda
Dobbymin <gmin0701@knu.ac.kr>
2024-10-19 21:22:54 +0900

    Refactor(hello-call-apply): 포인트 계산하기 기능 구현

--- src/pages/guard/hello-call-apply/components/service-total/ServiceTotal.tsx
+++ src/pages/guard/hello-call-apply/components/service-total/ServiceTotal.tsx
@@ -1,31 +1,66 @@
+import { useState } from 'react';
+
+import { usePostCostHelloCall } from '../../api/hooks';
+import { CostHelloCallRequest } from '../../api/types';
+import { TimeSlots } from '@/pages';
 import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 type Props = {
-  time: number;
-  count: number;
+  serviceTime: number;
+  startDate: Date | null;
+  endDate: Date | null;
+  timeSlotsArray: TimeSlots[];
+  setPrice: (price: number) => void;
 };
 
-export const ServiceTotal = ({ time, count }: Props) => {
-  const isButtonDisabled = time === 0;
+export const ServiceTotal = ({
+  serviceTime,
+  startDate,
+  endDate,
+  timeSlotsArray,
+  setPrice,
+}: Props) => {
+  const [totalServiceCount, setTotalServiceCount] = useState<number | null>(0);
+
+  const { mutate: postCostHelloCall } = usePostCostHelloCall();
+  const isButtonDisabled = serviceTime === 0;
+
+  const handleButtonClick = () => {
+    const requestPayload: CostHelloCallRequest = {
+      serviceTime,
+      startDate: startDate?.toISOString() || '',
+      endDate: endDate?.toISOString() || '',
+      timeSlots: timeSlotsArray,
+    };
+
+    postCostHelloCall(requestPayload, {
+      onSuccess: (data) => {
+        setPrice(data.price);
+        setTotalServiceCount(data.totalServiceCount);
+      },
+    });
+  };
+
   return (
     <ContentsBox>
       <TitleText>서비스 총 이용 횟수</TitleText>
       <Flex alignItems='center' justifyContent='center' gap={2} mb={3}>
         <Text as='b'>매주</Text>
-        <HighlightText>{time}분</HighlightText>
+        <HighlightText>{serviceTime}분</HighlightText>
         <Text as='b' mx='0.5'>
           씩
         </Text>
-        <HighlightText>{count}회</HighlightText>
+        <HighlightText>{totalServiceCount}회</HighlightText>
         <Text as='b'>서비스 이용</Text>
       </Flex>
       <Button
+        onClick={handleButtonClick}
         backgroundColor={isButtonDisabled ? '#e2e8e0' : 'var(--color-primary)'}
         color={isButtonDisabled ? '#a0aec0' : 'var(--color-white)'}
         isDisabled={isButtonDisabled}
       >
-        <Text color='var(--color-white)'>포인트 계산하기</Text>
+        포인트 계산하기
       </Button>
     </ContentsBox>
   );
