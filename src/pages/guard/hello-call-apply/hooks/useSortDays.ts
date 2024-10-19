0f3e2e534d0f339aef13d63225d28aee6e46714c
Dobbymin <gmin0701@knu.ac.kr>
2024-10-19 22:18:14 +0900

    Refactor(hello-call-apply): 요일을 기준으로 재 배열 hook 구현

--- src/pages/guard/hello-call-apply/hooks/useSortDays.ts
+++ src/pages/guard/hello-call-apply/hooks/useSortDays.ts
@@ -1,11 +1,24 @@
 import { useMemo } from 'react';
 
+import { TimeSlots } from '../HelloCallApplyPage';
 import { DAY_ORDER } from '../data';
 
-export const useSortDays = (daysArray: string[]) => {
-  return useMemo(() => {
-    return daysArray
-      .filter((day) => day)
-      .sort((a, b) => DAY_ORDER.indexOf(a) - DAY_ORDER.indexOf(b));
-  }, [daysArray]);
+// export const useSortDays = (daysArray: string[]) => {
+//   return useMemo(() => {
+//     return daysArray
+//       .filter((day) => day)
+//       .sort((a, b) => DAY_ORDER.indexOf(a) - DAY_ORDER.indexOf(b));
+//   }, [daysArray]);
+// };
+
+export const useSortDays = (timeSlotsArray: TimeSlots[]) => {
+  const sortedTimeSlotsArray = useMemo(() => {
+    return timeSlotsArray.slice().sort((a, b) => {
+      const dayA = DAY_ORDER.indexOf(a.dayName);
+      const dayB = DAY_ORDER.indexOf(b.dayName);
+      return dayA - dayB;
+    });
+  }, [timeSlotsArray]);
+
+  return sortedTimeSlotsArray;
 };
