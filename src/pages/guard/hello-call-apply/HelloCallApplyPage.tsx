8df32427992195faee38ddf3ec4d44e0fe95865a
Dobbymin <gmin0701@knu.ac.kr>
2024-10-19 21:18:27 +0900

    Refactor(hello-call-apply): 안부전화 서비스 지원하기 페이지 구조 수정 및 컴포넌트 분리

--- src/pages/guard/hello-call-apply/HelloCallApplyPage.tsx
+++ src/pages/guard/hello-call-apply/HelloCallApplyPage.tsx
@@ -1,43 +1,53 @@
 import { useState } from 'react';
 
+import { TimeSlot } from './api/types';
 import {
+  NoticeArea,
   SelectSenior,
   ServicePeriod,
   ServiceTime,
   ServiceTotal,
+  TellToSinitto,
 } from './components';
-import { NOTICE_DATA } from './data';
-import { Notice } from '@/shared';
-import { Box, Button, Text } from '@chakra-ui/react';
+import { Button, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
-export const HelloCallApplyPage = () => {
-  const [serviceTime, setServiceTime] = useState(0);
-  const [serviceCount, setServiceCount] = useState(1);
+export type TimeSlots = {
+  selectedTime?: number | null;
+} & TimeSlot;
 
+export const HelloCallApplyPage = () => {
+  const [timeSlotsArray, setTimeSlotsArray] = useState<TimeSlots[]>([]);
+  const [serviceTime, setServiceTime] = useState(0);
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [endDate, setEndDate] = useState<Date | null>(null);
+  const [price, setPrice] = useState<number | null>(null);
+
+  console.log(timeSlotsArray);
   return (
     <HelloCallApplyPageLayout>
-      <NoticeBox>
-        <Notice
-          noticeType={NOTICE_DATA.noticeType}
-          title={NOTICE_DATA.title_price}
-          contents={NOTICE_DATA.contents_price}
-        />
-      </NoticeBox>
-      <NoticeBox>
-        <Notice
-          noticeType={NOTICE_DATA.noticeType}
-          title={NOTICE_DATA.title_info}
-          contents={NOTICE_DATA.contents_info}
-        />
-      </NoticeBox>
+      <NoticeArea />
       <SelectSenior />
-      <ServiceTime setTime={setServiceTime} setCount={setServiceCount} />
-      <ServicePeriod />
-      <ServiceTotal time={serviceTime} count={serviceCount} />
-
-      <Button w='90%' backgroundColor='var(--color-primary)'>
-        <Text color='var(--color-white)'>2,000 point로 신청하기</Text>
+      <ServiceTime
+        setTimeSlotsArray={setTimeSlotsArray}
+        setServiceTime={setServiceTime}
+      />
+      <ServicePeriod
+        startDate={startDate}
+        setStartDate={setStartDate}
+        endDate={endDate}
+        setEndDate={setEndDate}
+      />
+      <ServiceTotal
+        startDate={startDate}
+        endDate={endDate}
+        timeSlotsArray={timeSlotsArray}
+        serviceTime={serviceTime}
+        setPrice={setPrice}
+      />
+      <TellToSinitto />
+      <Button w='90%' mt={5} backgroundColor='var(--color-primary)'>
+        <Text color='var(--color-white)'>{price} point로 신청하기</Text>
       </Button>
     </HelloCallApplyPageLayout>
   );
@@ -51,10 +61,4 @@ const HelloCallApplyPageLayout = styled.div`
   align-items: center;
 
   padding: 1rem 1.5rem;
-`;
-
-const NoticeBox = styled(Box)`
-  display: flex;
-  flex-direction: column;
-  margin: 0.5rem 0;
 `;
