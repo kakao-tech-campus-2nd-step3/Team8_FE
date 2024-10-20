6b1eaa76f0b0640ee34f6da24055b524f64adff1
Dobbymin <gmin0701@knu.ac.kr>
2024-10-20 12:23:26 +0900

    Refactor: 안부전화 신청 api 폴더 이동

--- src/pages/guard/hello-call-apply/api/hooks/index.ts
+++ src/pages/guard/hello-call-apply/api/hooks/index.ts
@@ -1,1 +1,2 @@
 export { usePostCostHelloCall } from './usePostCostHelloCall';
+export { usePostApplyHelloCall } from './usePostApplyHelloCall';
--- src/pages/guard/hello-call-apply/api/types/index.ts
+++ src/pages/guard/hello-call-apply/api/types/index.ts
@@ -1,2 +1,4 @@
 export type { CostHelloCallRequest, TimeSlot } from './cost-hello-call.request';
 export type { CostHelloCallResponse } from './cost-hello-call.response';
+export type { ApplyHelloCallRequest } from './apply-hello-call.reqeust';
+export type { ApplyHelloCallResponse } from './apply-hello-call.response';
