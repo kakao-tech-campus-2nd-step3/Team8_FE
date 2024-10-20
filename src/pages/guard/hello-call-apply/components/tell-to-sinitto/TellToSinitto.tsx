71cb684f341a7281ed05124a9dcfee3ab6bc947c
Dobbymin <gmin0701@knu.ac.kr>
2024-10-20 19:27:38 +0900

    Feat(hello-call-apply): 작성한 글 내보내기 기능 구현

--- src/pages/guard/hello-call-apply/components/tell-to-sinitto/TellToSinitto.tsx
+++ src/pages/guard/hello-call-apply/components/tell-to-sinitto/TellToSinitto.tsx
@@ -1,12 +1,28 @@
 import { Flex, Text, Textarea } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
-export const TellToSinitto = () => {
+type Props = {
+  message: string;
+  setMessage: (value: string) => void;
+};
+
+export const TellToSinitto = ({ message, setMessage }: Props) => {
+  const handleTextareaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMessage(event.target.value);
+  };
+
   return (
     <Wrapper flexDir='column'>
       <TitleText>시니또에게 전할 내용</TitleText>
       <Wrapper my={3}>
-        <Textarea h='10rem' placeholder='어르신에 대한 내용을 입력해주세요' />
+        <Textarea
+          h='10rem'
+          placeholder='어르신에 대한 내용을 입력해주세요'
+          value={message}
+          onChange={handleTextareaChange}
+        />
       </Wrapper>
     </Wrapper>
   );
