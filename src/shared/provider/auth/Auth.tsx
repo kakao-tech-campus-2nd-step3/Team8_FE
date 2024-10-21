4501dd1497b57d55b063a31f49f5d4e29669759b
Dobbymin <gmin0701@knu.ac.kr>
2024-10-22 01:40:19 +0900

    Refactor(auth-provider): email만 상태관리 하도록 수정

--- src/shared/provider/auth/Auth.tsx
+++ src/shared/provider/auth/Auth.tsx
@@ -1,44 +1,26 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-
-import { authLocalStorage } from '@/shared/utils/storage';
+import { createContext, ReactNode, useContext, useState } from 'react';
 
 type AuthInfo = {
-  accessToken: string;
-  refreshToken: string;
-  redirectUrl: string;
-  email: string;
+  email: string | null;
+  setEmail: (email: string) => void;
 };
 
 export const AuthContext = createContext<AuthInfo | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const currentAuthToken = authLocalStorage.get();
-  const [isReady, setIsReady] = useState(!currentAuthToken);
+  const [email, setEmail] = useState<string | null>(null);
 
-  const [authInfo, setAuthInfo] = useState<AuthInfo | undefined>(undefined);
-
-  useEffect(() => {
-    if (currentAuthToken) {
-      setAuthInfo({
-        accessToken: currentAuthToken,
-        refreshToken: currentAuthToken,
-        redirectUrl: currentAuthToken,
-        email: currentAuthToken,
-      });
-      setIsReady(true);
-    }
-  }, [currentAuthToken]);
-
-  if (!isReady) return <></>;
   return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ email, setEmail }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth는 AuthProvider 내부에서 사용되어야 합니다.');
+  }
+  return context;
+};
