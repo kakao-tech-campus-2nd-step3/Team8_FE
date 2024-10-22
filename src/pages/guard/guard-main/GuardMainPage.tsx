6d59e91be27bdda2f0a8ee1dae616afd2526457f
Dobbymin <gmin0701@knu.ac.kr>
2024-10-22 21:02:18 +0900

    Feat(guard-main): 보호자 메인 페이지 구현

--- src/pages/guard/guard-main/GuardMainPage.tsx
+++ src/pages/guard/guard-main/GuardMainPage.tsx
@@ -1,3 +1,22 @@
+import { GuideLineList } from './components/guideline-list';
+import { HelloCallApply } from './components/hello-call-apply';
+import { Flex } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
 export const GuardMainPage = () => {
-  return <div>GuardMainPage</div>;
+  return (
+    <GuardMainPageLayout>
+      <Flex>header</Flex>
+      <GuideLineList />
+      <HelloCallApply />
+    </GuardMainPageLayout>
+  );
 };
+
+const GuardMainPageLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 0 2rem;
+`;
