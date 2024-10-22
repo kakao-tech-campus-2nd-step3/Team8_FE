66f3f7a4d4dbc8d60407f18be9992d9ad64efe7e
Dobbymin <gmin990624@gmail.com>
2024-10-23 02:48:37 +0900

    Feat(guard-main): 안부전화 서비스 section 구현

--- src/pages/guard/guard-main/components/hello-call-apply/HelloCallApply.tsx
+++ src/pages/guard/guard-main/components/hello-call-apply/HelloCallApply.tsx
@@ -1,5 +1,5 @@
 import { IconArrow } from '@/pages/assets';
-import HelloCallImg from '@/pages/assets/guard-main/hello-call.svg';
+import HelloCallImg from '@/pages/assets/guard-main/hello-call.png';
 import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
@@ -9,8 +9,7 @@ export const HelloCallApply = () => {
       <NoticeTitle>안부 전화 서비스 이용하기</NoticeTitle>
       <NoticeText>안부 전화 서비스를 이용해보세요.</NoticeText>
       <NoticeText>다정한 시니또들이 대신 말 벗이 되어드립니다.</NoticeText>
-      <Flex w='100%' justifyContent='space-between'>
-        <Image src={HelloCallImg} alt='hello-call' />
+      <ContentWrapper>
         <ServiceApplyButton>
           <Flex flexDir='column' gap={1.5} alignItems='start'>
             <ButtonText>안부 전화 서비스</ButtonText>
@@ -18,7 +17,8 @@ export const HelloCallApply = () => {
           </Flex>
           <IconArrow fill='black' />
         </ServiceApplyButton>
-      </Flex>
+        <StyledImage src={HelloCallImg} alt='hello-call' />
+      </ContentWrapper>
     </Wrapper>
   );
 };
@@ -40,12 +40,28 @@ const NoticeText = styled(Text)`
   color: var(--color-gray);
 `;
 
+const ContentWrapper = styled(Flex)`
+  position: relative;
+  width: 100%;
+  justify-content: flex-end;
+  gap: 2rem;
+  height: 150px;
+`;
+
+const StyledImage = styled(Image)`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 110px;
+  height: 110px;
+`;
+
 const ServiceApplyButton = styled(Button)`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   border-radius: 0.7rem;
-  margin: 1rem 0;
+  margin: 1.8rem 0;
   height: 5rem;
   width: 11rem;
   background-color: #f0ebe3;
