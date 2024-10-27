ae4179e9d4d7624197edb43050398b646316436f
unknown <kc21ynje@gmail.com>
2024-10-28 02:44:12 +0900

    Fix(call-back): 코드리뷰 반영 (type 이름 변경)

--- src/pages/sinitto/call-back/list/api/get-call-backs.api.ts
+++ src/pages/sinitto/call-back/list/api/get-call-backs.api.ts
@@ -1,10 +1,10 @@
-import type { CallbacksResponse } from './types';
+import type { CallbackListResponse } from './types';
 import { fetchInstance } from '@/shared/api/instance';
 
 const getCallbacksPath = () => `/api/callbacks`;
 
 export const getCallbacks = async (page: number, size: number) => {
-  const response = await fetchInstance.get<CallbacksResponse>(
+  const response = await fetchInstance.get<CallbackListResponse>(
     getCallbacksPath(),
     {
       params: {
