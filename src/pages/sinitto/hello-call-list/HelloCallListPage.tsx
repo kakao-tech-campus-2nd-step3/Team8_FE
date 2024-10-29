ffe09c62bcdc0435a4a92b4f49ed0c1a742acc65
Dobbymin <gmin990624@gmail.com>
2024-10-30 04:52:21 +0900

    Fix(hello-call): 무한스크롤 기능 수정

--- src/pages/sinitto/hello-call-list/HelloCallListPage.tsx
+++ src/pages/sinitto/hello-call-list/HelloCallListPage.tsx
@@ -1,18 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useGetServiceList } from './api';
 import { CallRequest } from './components';
 import { RouterPath } from '@/app/routes/path';
-import { LoadingView, VisibilityLoader } from '@/shared/components';
+import { LoadingView } from '@/shared/components';
 import { Flex, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 const HelloCallListPage = () => {
   const [isLastPageReached, setIsLastPageReached] = useState(false);
+  const observerRef = useRef<IntersectionObserver | null>(null);
 
   const { data, isError, isLoading, hasNextPage, fetchNextPage, error } =
-    useGetServiceList();
+    useGetServiceList(10);
 
   const navigate = useNavigate();
 
@@ -21,6 +22,23 @@ const HelloCallListPage = () => {
   const handlerNavigate = (helloCallId: number) => {
     navigate(`${RouterPath.HELLO_CALL_SERVICE}/${helloCallId}`);
   };
+
+  const lastElementRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (isLoading) return;
+
+      if (observerRef.current) observerRef.current.disconnect();
+
+      observerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          fetchNextPage();
+        }
+      });
+
+      if (node) observerRef.current.observe(node);
+    },
+    [isLoading, fetchNextPage, hasNextPage]
+  );
 
   useEffect(() => {
     if (isError && error instanceof Error && error.message.includes('500')) {
@@ -37,17 +55,18 @@ const HelloCallListPage = () => {
       <Flex w='100%' justifyContent='end'>
         <Text>새로고침</Text>
       </Flex>
-      {allContent.map((item) => (
-        <CallRequest
-          key={item.helloCallId}
-          seniorName={item.seniorName}
-          days={item.days}
-          onClick={() => handlerNavigate(item.helloCallId)}
-        />
-      ))}
-      {hasNextPage && !isLoading && !isLastPageReached && (
-        <VisibilityLoader callback={fetchNextPage} />
-      )}
+      {allContent.map((item, index) => {
+        const isLastElement = index === allContent.length - 1; // 마지막 요소인지 확인
+        return (
+          <CallRequest
+            key={item.helloCallId}
+            seniorName={item.seniorName}
+            days={item.days}
+            onClick={() => handlerNavigate(item.helloCallId)}
+            ref={isLastElement ? lastElementRef : null} // 마지막 요소에 ref 할당
+          />
+        );
+      })}
       {(!hasNextPage || isLastPageReached) && (
         <Text>더 이상 요청이 없어요 🥲</Text>
       )}
