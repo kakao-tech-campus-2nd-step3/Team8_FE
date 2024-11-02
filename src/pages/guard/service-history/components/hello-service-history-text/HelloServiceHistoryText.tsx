e2649fd5335c66be21a04f226b2770f27d1851e9
diwoni <70441308+Diwoni@users.noreply.github.com>
2024-11-03 07:51:55 +0900

    Style: 안부전화 서비스 이용내역 설명 텍스트 수정

--- src/pages/guard/service-history/components/hello-service-history-text/HelloServiceHistoryText.tsx
+++ src/pages/guard/service-history/components/hello-service-history-text/HelloServiceHistoryText.tsx
@@ -9,12 +9,21 @@ const HelloServiceHistoryText = () => {
           안부전화 이용 내역
         </Text>
       </Box>
-      <Box display='flex' justifyContent='flex-end' w='100%' flexGrow={1}>
+      <Box display='flex' flexDir='column' w='100%' flexGrow={1}>
         <Text fontSize={14} fontWeight={700} color='#6D6D6D' textAlign='right'>
           <Highlight>완료 대기</Highlight> 상태 서비스 항목을 클릭하면 서비스
           완료 확인과 봉사자에 대한 <Highlight>평가</Highlight>를 남길 수
-          있습니다. <Highlight>대기중</Highlight>인 서비스 항목은 요일만 수정할
-          수 있습니다. 이는 시니또에게 서비스에 대한{' '}
+          있습니다.
+        </Text>
+        <Text
+          mt={1}
+          fontSize={14}
+          fontWeight={700}
+          color='#6D6D6D'
+          textAlign='right'
+        >
+          <Highlight>대기중</Highlight>인 서비스 항목은 요일만 수정할 수
+          있습니다. 이는 시니또에게 서비스에 대한{' '}
           <Highlight>최소한의 일관성</Highlight>을 보장하기 위함임을
           이해바랍니다.
         </Text>
