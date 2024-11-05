9980ddf8122b368977eab7e3705fad6ab72e6139
Dobbymin <gmin990624@gmail.com>
2024-11-05 21:43:29 +0900

    Fix(router): 안부전화 보고서 경로 수정

--- src/app/routes/path.ts
+++ src/app/routes/path.ts
@@ -11,7 +11,7 @@ export const RouterPath = {
   SERVICE_HISTORY: `service-history`,
   HELLO_CALL: `hello-call`,
   HELLO_CALL_SERVICE: `:helloCallId`,
-  HELLO_CALL_REPORT: `report`,
+  HELLO_CALL_REPORT: `:helloCallId/report`,
   HELLO_CALL_GUARD_APPLY: `apply`,
   CALL_BACK_LIST: `call-back`,
   CALL_BACK_DETAIL: `:callBackId`,
