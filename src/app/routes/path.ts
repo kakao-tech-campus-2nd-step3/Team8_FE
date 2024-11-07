aced1ee63925ff63c414c3a8d90d972e8ddd3eba
Dobbymin <gmin990624@gmail.com>
2024-11-07 15:31:44 +0900

    Feat(router): 시니또 서비스 이용내역 페이지 경로 상수화 및 보고서 쓰기 경로 수정

--- src/app/routes/path.ts
+++ src/app/routes/path.ts
@@ -11,13 +11,14 @@ export const RouterPath = {
   SERVICE_HISTORY: `service-history`,
   HELLO_CALL: `hello-call`,
   HELLO_CALL_SERVICE: `:helloCallId`,
-  HELLO_CALL_REPORT: `:helloCallId/report`,
+  HELLO_CALL_REPORT: `report/:helloCallId`,
   HELLO_CALL_GUARD_APPLY: `apply`,
   CALL_BACK_LIST: `call-back`,
   CALL_BACK_DETAIL: `:callBackId`,
   CALL_BACK_GUID_LINE: `:guideLineId`,
   SENIOR_REGISTER: `/senior-register`,
   SINITTO_REVIEW: `review`,
+  SINITTO_SERVICE_HISTORY: `service-history`,
 
   DUMMY_LOGIN: `/dummy`,
 };
