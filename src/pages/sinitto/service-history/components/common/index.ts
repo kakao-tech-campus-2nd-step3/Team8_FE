1a418e5d34048bbd9c30b66656e8b21778caa246
Dobbymin <gmin990624@gmail.com>
2024-11-07 16:31:39 +0900

    Feat(service-history): 시니또 진행중인 서비스 기록 페이지 구현

--- src/pages/sinitto/service-history/components/common/index.ts
+++ src/pages/sinitto/service-history/components/common/index.ts
@@ -1,3 +1,2 @@
-export * from './text-area';
 export * from './call-back-service-list';
 export * from './hello-call-service-list';
---
+++ src/pages/sinitto/service-history/index.ts
@@ -0,0 +1,1 @@
+export { SinittoServiceHistoryPage } from './SinittoServiceHistory';
