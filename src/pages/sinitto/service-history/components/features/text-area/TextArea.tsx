0556305c9c9eb1863111c5b693cabeae65b3e149
Dobbymin <gmin990624@gmail.com>
2024-11-07 16:20:48 +0900

    Feat(service-history): 서비스 리스트 설명 글 기능 구현

---
+++ src/pages/sinitto/service-history/components/features/text-area/TextArea.tsx
@@ -0,0 +1,41 @@
+import { useArrayFormatting } from '../../../hooks';
+import { Flex, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+type Props = {
+  title: string;
+  status: string | string[];
+  description: string | string[];
+  textDirection?: 'start' | 'end';
+};
+
+export const TextArea = ({
+  title,
+  status,
+  description,
+  textDirection = 'start',
+}: Props) => {
+  const statuses = useArrayFormatting(status);
+  const descriptions = useArrayFormatting(description);
+
+  return (
+    <Flex w='full' flexDir='column' textAlign={textDirection}>
+      <Flex mt='1rem' w='full' justifyContent={textDirection}>
+        <Text fontSize='xl' fontWeight='700'>
+          {title}
+        </Text>
+      </Flex>
+      {statuses.map((stat, index) => (
+        <Flex w='full' my={2} justifyContent={textDirection} key={index}>
+          <Text fontSize='sm' fontWeight='700' color='#6D6D6D'>
+            <Highlight>{stat}</Highlight> {descriptions[index] || ''}
+          </Text>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
+
+const Highlight = styled.span`
+  color: #990e0e;
+`;
