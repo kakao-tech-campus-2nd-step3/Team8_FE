7d73550a1d7e7ef0fa28799f05205ad3d2208afa
Dobbymin <gmin990624@gmail.com>
2024-11-07 18:04:44 +0900

    Feat(hello-call-report): 보고서 제출 api 기능 구현

--- src/pages/sinitto/hello-call-report/api/write-report.api.ts
+++ src/pages/sinitto/hello-call-report/api/write-report.api.ts
@@ -1,11 +1,14 @@
 import { WriteReportRequest } from './types';
 import { fetchInstance } from '@/shared/api/instance';
 
-export const postWriteReportPath = () => `/api/hellocalls/reports}`;
+export const postWriteReportPath = () => `/api/hellocalls/reports`;
 
-export const postWriteReport = async (): Promise<WriteReportRequest> => {
+export const postWriteReport = async (
+  data: WriteReportRequest
+): Promise<WriteReportRequest> => {
   const response = await fetchInstance.post<WriteReportRequest>(
-    postWriteReportPath()
+    postWriteReportPath(),
+    data
   );
   return response.data;
 };
