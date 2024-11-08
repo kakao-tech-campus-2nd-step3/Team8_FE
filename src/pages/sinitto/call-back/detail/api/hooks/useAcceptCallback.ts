9ec62bc5f1aae67e1180847fb26fb234ef904db7
unknown <kc21ynje@gmail.com>
2024-11-08 14:00:00 +0900

    Fix(call-back): 빈 함수 삭제

--- src/pages/sinitto/call-back/detail/api/hooks/useAcceptCallback.ts
+++ src/pages/sinitto/call-back/detail/api/hooks/useAcceptCallback.ts
@@ -4,7 +4,6 @@ import { useMutation } from '@tanstack/react-query';
 export const useAcceptCallback = () => {
   return useMutation({
     mutationFn: acceptCallback,
-    onSuccess: () => {},
     onError: () => {
       alert(`콜백 요청 수락 신청 중 오류가 발생했습니다`);
     },
