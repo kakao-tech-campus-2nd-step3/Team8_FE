bd444422a08aa2a549ef4f95558afeae9551fa1b
unknown <kc21ynje@gmail.com>
2024-11-08 12:00:28 +0900

    Fix(call-back): 콜백 서비스 취소 시 발생하는 오류 해결 (경로 수정)

--- src/pages/sinitto/call-back/detail/api/hooks/useAcceptCallback.ts
+++ src/pages/sinitto/call-back/detail/api/hooks/useAcceptCallback.ts
@@ -4,9 +4,7 @@ import { useMutation } from '@tanstack/react-query';
 export const useAcceptCallback = () => {
   return useMutation({
     mutationFn: acceptCallback,
-    onSuccess: () => {
-      alert('콜백 요청이 수락되었습니다.');
-    },
+    onSuccess: () => {},
     onError: (error) => {
       alert(`콜백 요청 수락 신청 중 오류가 발생했습니다: ${error.message}`);
     },
--- src/pages/sinitto/call-back/detail/components/menu/CallbackMenu.tsx
+++ src/pages/sinitto/call-back/detail/components/menu/CallbackMenu.tsx
@@ -48,7 +48,7 @@ export const CallbackMenu = ({
     isSuccess: isCancelSuccess,
   } = useCancelCallback();
   if (isCancelSuccess) {
-    navigate(RouterPath.CALL_BACK_LIST);
+    navigate(RouterPath.SINITTO + '/' + RouterPath.CALL_BACK_LIST);
   }
 
   const isLoading = isAcceptLoading || isCancelLoading || isCompleteLoading;
