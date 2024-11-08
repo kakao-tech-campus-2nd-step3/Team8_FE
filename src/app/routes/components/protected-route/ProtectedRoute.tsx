6c17b29876f1a532ff66f8e5f698adc3ba58d8e4
unknown <kc21ynje@gmail.com>
2024-11-08 12:27:40 +0900

    Fix: import 수정

--- src/app/routes/components/protected-route/ProtectedRoute.tsx
+++ src/app/routes/components/protected-route/ProtectedRoute.tsx
@@ -1,5 +1,4 @@
-import { Navigate } from 'react-router-dom';
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 import { RouterPath } from '../../path';
 
