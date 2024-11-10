5eb5a2e991a59dd25fbfbc10e6c5df498bd1635a
Dobbymin <gmin990624@gmail.com>
2024-11-11 01:36:34 +0900

    Feat(pwa): vite-pwa 구현

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,50 @@ export default defineConfig({
   define: {
     global: {},
   },
-  plugins: [react(), , basicSsl(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    react(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      includeAssets: [
+        'favicon.ico',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+      ],
+      manifest: {
+        name: '나만의 작은 시니또',
+        short_name: '시니또',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#485EEE',
+        icons: [
+          {
+            src: 'icons/favicon.ico',
+            sizes: '64x64 32x32 24x24 16x16',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'icons/icon-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -19,6 +62,14 @@ export default defineConfig({
   server: {
     hmr: {
       overlay: false,
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        sw: './service-worker.js',
+      },
     },
   },
 });
