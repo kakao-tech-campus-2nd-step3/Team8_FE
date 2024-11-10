c75267a8bfeca14f2d2518876a1e08073c393d3f
Dobbymin <gmin990624@gmail.com>
2024-11-10 21:41:01 +0900

    Fix: 안부전화 상세페이지 분리

---
+++ src/pages/sinitto/hello-call-detail/api/types/service-detail.response.ts
@@ -0,0 +1,23 @@
+type Time = {
+  hour: number;
+  minute: number;
+  second: number;
+  nano: number;
+};
+
+export type TimeSlot = {
+  dayName: string;
+  startTime: Time;
+  endTime: Time;
+};
+
+export type ServiceDetailResponse = {
+  startDate: string;
+  endDate: string;
+  timeSlots: TimeSlot[];
+  requirement: string;
+  seniorName: string;
+  seniorPhoneNumber: string;
+  serviceTime: number;
+  price: number;
+};
--- src/pages/sinitto/hello-call-service/HelloCallServicePage.tsx
+++ src/pages/sinitto/hello-call-service/HelloCallServicePage.tsx
@@ -1,18 +1,15 @@
-import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useGetServiceDetail, usePutAcceptHelloCall } from './api';
-import TitleImg from './assets/title-icon.png';
 import { ServiceDetail } from './components';
 import { SERVICE_NOTICE } from './data';
-import { useFormatPhoneNumber, useServiceDate } from './hooks';
+import { useServiceDate } from './hooks';
+import TitleImg from '@/pages/assets/shared/hello-call/title-icon.png';
 import { Notice } from '@/shared/components';
-import { Box, Button, Divider, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Image } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 
 const HelloCallServicePage = () => {
-  const [detailed, setDetailed] = useState(false);
-
   const { helloCallId } = useParams();
 
   const { data } = useGetServiceDetail(Number(helloCallId));
@@ -24,44 +21,21 @@ const HelloCallServicePage = () => {
   const startDate = useServiceDate(data?.startDate);
   const endDate = useServiceDate(data?.endDate);
 
-  const phoneNumber = useFormatPhoneNumber(data?.seniorPhoneNumber);
-
-  const goToDetail = () => {
-    setDetailed(true);
-  };
-
   const handleAcceptService = () => {
     acceptHelloCall();
   };
 
   return (
     <HelloCallServicePageLayout>
-      {!detailed ? (
-        <Box
-          display='flex'
-          py={3}
-          justifyContent='center'
-          backgroundColor='#E6DFD1'
-          borderRadius='0.5rem'
-        >
-          <Image src={TitleImg} alt='title-img' />
-        </Box>
-      ) : (
-        <Box display='flex' flexDir='column' alignItems='center'>
-          <Box
-            display='flex'
-            w='10rem'
-            justifyContent='center'
-            backgroundColor='var(--color-secondary)'
-            borderRadius='5px'
-          >
-            <Text color='var(--color-primary)'>시니어 전화번호</Text>
-          </Box>
-          <Text fontSize='var(--font-size-xl)' fontWeight='700'>
-            {phoneNumber}
-          </Text>
-        </Box>
-      )}
+      <Box
+        display='flex'
+        py={3}
+        justifyContent='center'
+        backgroundColor='#E6DFD1'
+        borderRadius='0.5rem'
+      >
+        <Image src={TitleImg} alt='title-img' />
+      </Box>
       <ServiceDetail
         startDate={startDate}
         endDate={endDate}
@@ -75,24 +49,10 @@ const HelloCallServicePage = () => {
           contents={SERVICE_NOTICE.service_contents}
           noticeType='안부전화'
         />
-        {detailed && (
-          <>
-            <Notice
-              title={SERVICE_NOTICE.finish_title}
-              contents={SERVICE_NOTICE.finish_contents}
-              noticeType='안부전화'
-            />
-            <Divider />
-          </>
-        )}
       </Box>
-      {!detailed ? (
-        <AcceptButton onClick={goToDetail}>서비스 상세 확인하기</AcceptButton>
-      ) : (
-        <AcceptButton onClick={handleAcceptService}>
-          서비스 수락하기 ({data?.price.toLocaleString()}P)
-        </AcceptButton>
-      )}
+      <AcceptButton onClick={handleAcceptService}>
+        서비스 수락하기 ({data?.price.toLocaleString()}P)
+      </AcceptButton>
     </HelloCallServicePageLayout>
   );
 };
