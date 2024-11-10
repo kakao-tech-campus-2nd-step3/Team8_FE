b8bae1cf5686be55d3d473e1f75d98e6ce514e85
diwoni <70441308+Diwoni@users.noreply.github.com>
2024-11-10 11:41:23 +0900

    Refactor: 콜백 서비스 완료 처리 후 콜백 서비스 이용내역 갱신

--- src/pages/guard/service-history/api/hooks/useCompleteCallback.ts
+++ src/pages/guard/service-history/api/hooks/useCompleteCallback.ts
@@ -1,4 +1,6 @@
+import { getCallbackHistoryQueryKey } from '../get-callback-history.api';
 import { completeCallback } from '../make-callback-completed.api';
+import { queryClient } from '@/shared';
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
 
 export const useCompleteCallback = (): UseMutationResult<
@@ -8,6 +10,12 @@ export const useCompleteCallback = (): UseMutationResult<
 > => {
   return useMutation({
     mutationFn: (callbackId: number) => completeCallback(callbackId),
+    onSuccess: () => {
+      alert('콜백 서비스를 완료 처리하였습니다.');
+      queryClient.invalidateQueries({
+        queryKey: getCallbackHistoryQueryKey(1),
+      });
+    },
     onError: (error: Error) => {
       console.error(error);
     },
