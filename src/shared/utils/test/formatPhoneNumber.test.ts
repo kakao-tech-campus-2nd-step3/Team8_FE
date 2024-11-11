a3d10407bf87844dfd76bda0c51715462c6ecbde
diwoni <70441308+Diwoni@users.noreply.github.com>
2024-11-11 18:49:22 +0900

    Test: 전화번호 유틸 함수 관련 테스트 코드 추가

--- src/shared/utils/test/formatPhoneNumber.test.ts
+++ src/shared/utils/test/formatPhoneNumber.test.ts
@@ -1,4 +1,4 @@
-import { formatPhoneNumber } from '../formatPhoneNumber';
+import { formatPhoneNumber, parsePhoneNumber } from '../formatPhoneNumber';
 
 describe('> formatPhoneNumber', () => {
   describe('formatPhoneNumber', () => {
@@ -19,6 +19,35 @@ describe('> formatPhoneNumber', () => {
     it('잘못된 형식의 전화번호는 원본을 반환한다', () => {
       expect(formatPhoneNumber('123')).toBe('123');
       expect(formatPhoneNumber('300포인트')).toBe('300포인트');
+    });
+  });
+});
+
+describe('> parsePhoneNumber', () => {
+  describe('전화번호 문자열에서 숫자만 추출한다', () => {
+    it.each([
+      {
+        phone: '010-1234-5678',
+        expected: '01012345678',
+      },
+      {
+        phone: '055-339-6717',
+        expected: '0553396717',
+      },
+      {
+        phone: '010.1234.5678',
+        expected: '01012345678',
+      },
+      {
+        phone: '010 1234 5678',
+        expected: '01012345678',
+      },
+      {
+        phone: '01012345678',
+        expected: '01012345678',
+      },
+    ])('phone: $phone => $expected', ({ phone, expected }) => {
+      expect(parsePhoneNumber(phone)).toBe(expected);
     });
   });
 });
