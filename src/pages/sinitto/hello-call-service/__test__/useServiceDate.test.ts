362f8b21df84d51eefc79db3d7bbaaa0567c022b
Dobbymin <gmin990624@gmail.com>
2024-11-12 01:42:05 +0900

    Fix: 테스트 코드 수정

--- src/pages/sinitto/hello-call-service/__test__/useServiceDate.test.ts
+++ src/pages/sinitto/hello-call-service/__test__/useServiceDate.test.ts
@@ -1,18 +1,26 @@
-import { useServiceDate } from '../hooks';
+import { useServiceDate } from '../hooks/useServiceDate';
 
-describe('useServiceDate', () => {
-  it('올바른 날짜 형식을 포맷하여 반환한다', () => {
-    const formattedDate = useServiceDate('1981-02-22T09:00:00');
-    expect(formattedDate).toBe('2월 22일');
-  });
-
-  it('한 자리 월과 일에도 올바르게 포맷하여 반환한다', () => {
-    const formattedDate = useServiceDate('2023-01-05T12:00:00');
-    expect(formattedDate).toBe('1월 05일');
-  });
-
-  it('서비스 날짜가 undefined일 경우 빈 문자열을 반환한다', () => {
-    const formattedDate = useServiceDate(undefined);
-    expect(formattedDate).toBe('');
+describe('> useServiceDate', () => {
+  describe("날짜 형식을 'YYYY-MM-DDTHH:mm:ss' 형식에서 'MM월 DD일' 형식으로 변환한다.", () => {
+    it.each([
+      {
+        date: '1981-02-22T09:00:00',
+        expected: '2월 22일',
+      },
+      {
+        date: '2023-01-05T12:00:00',
+        expected: '1월 05일',
+      },
+      {
+        date: undefined,
+        expected: '',
+      },
+      {
+        date: '2024-11-30T18:30:00',
+        expected: '11월 30일',
+      },
+    ])('date: $date => $expected', ({ date, expected }) => {
+      expect(useServiceDate(date)).toBe(expected);
+    });
   });
 });
