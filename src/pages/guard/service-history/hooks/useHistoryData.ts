439d0722d0d61a4466ca8365dfd833862782f272
diwoni <70441308+Diwoni@users.noreply.github.com>
2024-11-14 19:10:47 +0900

    Fix: 콜백 서비스 완료대기 -> 완료 일 때, 콜백 내역 refetch

--- src/pages/guard/service-history/hooks/useHistoryData.ts
+++ src/pages/guard/service-history/hooks/useHistoryData.ts
@@ -1,15 +1,14 @@
 import { useGetCallbackHistory, useGetHelloHistoryList } from '../hooks';
 
 export const useHistoryData = (currentPage: number, pageSize: number) => {
-  const { data: callbackHistory } = useGetCallbackHistory(
-    currentPage,
-    pageSize
-  );
+  const { data: callbackHistory, refetch: refetchCallback } =
+    useGetCallbackHistory(currentPage, pageSize);
   const { data: helloCallHistory, refetch } = useGetHelloHistoryList();
 
   return {
     callbackHistory,
     helloCallHistory,
     refetch,
+    refetchCallback,
   };
 };
