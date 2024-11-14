5c18ae212d6886fd4e032afdc586ad3a0e9c970d
Dobbymin <gmin990624@gmail.com>
2024-11-14 21:44:37 +0900

    Refactor: 전체 구조 수정

--- src/pages/sinitto/call-back/detail/hooks/useAcceptCallback.ts
+++ src/pages/sinitto/call-back/detail/hooks/useAcceptCallback.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from 'axios';
 
-import { acceptCallback } from '../accept-call-back.api';
+import { acceptCallback } from '../apis';
 import { useMutation } from '@tanstack/react-query';
 
 type ErrorResponse = {
--- src/pages/sinitto/call-back/detail/hooks/useCompleteCallback.ts
+++ src/pages/sinitto/call-back/detail/hooks/useCompleteCallback.ts
@@ -1,4 +1,4 @@
-import { CompleteCallback } from '../complete-call-back.api';
+import { CompleteCallback } from '../apis';
 import { useMutation } from '@tanstack/react-query';
 
 export const useCompleteCallback = () => {
