b693256275b351e21fc4f0001e3a9c50b9949989
Dobbymin <gmin990624@gmail.com>
2024-11-14 16:46:42 +0900

    Refactor: 명칭 변경 (봉사자 -> 시니또)

--- src/pages/guard/service-history/components/hello-service-history-text/HelloServiceHistoryText.tsx
+++ src/pages/guard/service-history/components/hello-service-history-text/HelloServiceHistoryText.tsx
@@ -12,7 +12,7 @@ const HelloServiceHistoryText = () => {
       <Box display='flex' w='100%' flexGrow={1}>
         <Text fontSize={14} fontWeight={700} color='#6D6D6D' textAlign='right'>
           <Highlight>완료 대기</Highlight> 상태 서비스 항목을 클릭하면 서비스
-          완료 확인과 봉사자에 대한 <Highlight>평가</Highlight>를 남길 수
+          완료 확인과 시니또에 대한 <Highlight>평가</Highlight>를 남길 수
           있습니다.
         </Text>
       </Box>
