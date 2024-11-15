14590503d55a567bebea101a358decd0c3e34968
unknown <kc21ynje@gmail.com>
2024-11-15 14:52:02 +0900

    Hotfix: 배포페이지에서 새로고침 시 루트 페이지가 보이는 오류 해결

--- src/app/routes/components/protected-route/ProtectedRoute.tsx
+++ src/app/routes/components/protected-route/ProtectedRoute.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 import { RouterPath } from '../../path';
@@ -10,8 +11,17 @@ type Props = {
 };
 
 const ProtectedRoute = ({ requiresAuth, sinittoOnly, guardOnly }: Props) => {
+  const [isAuthLoading, setIsAuthLoading] = useState(true);
   const accessToken = authStorage.accessToken.get();
   const isSinitto = authStorage.isSinitto.get() === 'true';
+
+  useEffect(() => {
+    setIsAuthLoading(false);
+  }, [accessToken, isSinitto]);
+
+  if (isAuthLoading) {
+    return <></>;
+  }
 
   if (requiresAuth && !accessToken) {
     return <Navigate to={RouterPath.ROOT} />;
